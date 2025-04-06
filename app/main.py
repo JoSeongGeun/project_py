1c02f3eeb6112f727a549ed076f35a67dcc35357
JoSeongGeun <chosg9591@gmail.com>
2025-04-06 17:33:17 +0900

    q

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from app.schema import SurveyInput
 from app.model import WeddingRecommender
+import os
 
+if __name__ != "__main__":
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
+    
 app = FastAPI()
 recommender = WeddingRecommender()
 
