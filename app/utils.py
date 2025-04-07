e5554203446532015196c80a9088c9ca9ec23c4f
JoSeongGeun <chosg9591@gmail.com>
2025-04-07 10:57:25 +0900

    비슷한 단어로 추천해주기

--- app/utils.py
+++ app/utils.py
@@ -10,6 +10,12 @@ def get_avg_word2vec_vector(keywords, model):
     for word in keywords:
         if word in model.wv:
             vectors.append(model.wv[word])
+        else:
+            # 모델에 없으면 가장 유사한 단어 하나 추천해서 사용
+            alt = suggest_similar(word, model)
+            if alt and alt in model.wv:
+                print(f"💡 '{word}' 대신 '{alt}' 사용됨")
+                vectors.append(model.wv[alt])
     if vectors:
         return np.mean(vectors, axis=0)
     else:
@@ -19,3 +25,10 @@ def get_corpus_avg_vectors(corpus, model):
     return np.array([
         get_avg_word2vec_vector(doc, model) for doc in corpus
     ])
+
+def suggest_similar(word, model):
+    try:
+        similar = model.most_similar(positive=[word], topn=1)
+        return similar[0][0]  # 가장 유사한 단어 하나 반환
+    except:
+        return None
